3ba1fb0fe7726b366cf94a3b6ad01d35ee52f620
Ronni Hartung <ronn2807@easv365.dk>
2015-09-25 13:45:18 +0200

    Ændringer i .net udgave, så den kan kører på webhotel

---
+++ MovieStoreUI/AppEvent.aspx
@@ -0,0 +1,6 @@
+﻿<script language=C# runat="server">
+	void Page_Load(object sender, System.EventArgs e)
+	{
+		throw(new ArgumentNullException());
+	}
+</script>
--- MovieStoreUI/Global.asax.cs
+++ MovieStoreUI/Global.asax.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
-
+using System.Diagnostics;
 
 namespace MovieStoreUI
 {
@@ -19,6 +19,18 @@ namespace MovieStoreUI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception objErr = Server.GetLastError().GetBaseException();
+            string err = "Error Caught in Application_Error event\n" +
+                    "Error in: " + Request.Url.ToString() +
+                    "\nError Message:" + objErr.Message.ToString() +
+                    "\nStack Trace:" + objErr.StackTrace.ToString();
+            EventLog.WriteEntry("Sample_WebApp", err, EventLogEntryType.Error);
+            //Server.ClearError();
+            //additional actions...
         }
     }
 }
