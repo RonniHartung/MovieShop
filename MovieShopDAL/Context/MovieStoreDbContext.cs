b093259f37a39ac085013a7cc813ed3c3f329f5d
unknown <post@kristjanthor.dk>
2015-10-21 09:43:14 +0200

    Sh cart works

--- MovieShopDAL/Context/MovieStoreDbContext.cs
+++ MovieShopDAL/Context/MovieStoreDbContext.cs
@@ -27,6 +27,5 @@ namespace MovieShopDAL
         public System.Data.Entity.DbSet<Order> Orders { get; set; }
         public System.Data.Entity.DbSet<OrderContent> OrderContents {get; set;}
 
-       // public System.Data.Entity.DbSet<MovieStoreUI.Models.ShoppingCartItem> ShoppingCartItems { get; set; }
     }
 }
